56c206453ea5206f7a5eb95abfde147969b9a5a3
Sabina Jarabkova <sabina@MacBook-Air-uzivatela-Sabina.local>
2023-02-08 19:18:07 +0100

    homework done

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 // tady je místo pro náš program
 
+let output = document.querySelector('.vystup');
 
+let firstName = String(prompt('Your first name is?'));
+let lastName = String(prompt('Your last name is?'));
+let yearOfBirth = Number(prompt('Your year of birth is?'))
 
+output.innerHTML = firstName + ' ' + lastName + '-' + yearOfBirth;
 
+let favoriteColor = String(prompt('Your favorite color is - please, in english?'));
+output.style.color = favoriteColor; 
