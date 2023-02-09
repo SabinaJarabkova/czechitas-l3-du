2a97bcc946737fc470734203d319718e9a3a272e
Sabina Jarabkova <sabina@MacBook-Air-uzivatela-Sabina.local>
2023-02-09 21:51:54 +0100

    uprava - zobrazuje vek, ne rok narozeni

--- main.js
+++ main.js
@@ -5,8 +5,11 @@ let output = document.querySelector('.vystup');
 let firstName = String(prompt('Your first name is?'));
 let lastName = String(prompt('Your last name is?'));
 let yearOfBirth = Number(prompt('Your year of birth is?'))
+let actualYear = 2023
+let age = actualYear - yearOfBirth
 
-output.innerHTML = firstName + ' ' + lastName + '-' + yearOfBirth;
+
+output.innerHTML = firstName + ' ' + lastName + ' - ' + age;
 
 let favoriteColor = String(prompt('Your favorite color is - please, in english?'));
 output.style.color = favoriteColor; 
